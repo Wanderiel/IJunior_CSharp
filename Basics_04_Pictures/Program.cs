47a5042fdd3eaf47aedc6aabbd4048d70cfe5d1f
Pavel Dymov <dpavel82@yandex.ru>
2024-10-01 10:29:30 +0300

    Картинки

---
+++ Basics_04_Pictures/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Basics_04_Pictures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxImagesInRow = 3;
+            int imagesCount = 52;
+            int filledRows = imagesCount / maxImagesInRow;
+            int imagesRemains = imagesCount % maxImagesInRow;
+
+            Console.WriteLine($"У вас есть картинки - {imagesCount} шт., в ряд помещается - {maxImagesInRow} шт." +
+                $"\nБудет заполнено рядов полностью - {filledRows}, в остатке картинок - {imagesRemains} шт.");
+
+            Console.ReadKey();
+        }
+    }
+}
