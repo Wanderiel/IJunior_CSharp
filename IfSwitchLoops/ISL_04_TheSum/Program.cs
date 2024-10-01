e9ff7becee1c15be36e8f3bb544a8194a8b28adf
Pavel Dymov <dpavel82@yandex.ru>
2024-10-01 12:29:24 +0300

    Сумма чисал

---
+++ IfSwitchLoops/ISL_04_TheSum/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ISL_04_TheSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int maxNumber = 100;
+            int number = random.Next(maxNumber);
+            int divider1 = 3;
+            int divider2 = 5;
+            int sum = 0;
+
+            for (int i = 0; i <= number; i++)
+            {
+                if (i % divider1 == 0 || i % divider2 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine($"Сумма всех положительных чисел меньше {number} (включая это число)," +
+                $" которые кратны {divider1} или {divider2} равна: {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
