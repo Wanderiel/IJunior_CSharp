6c3b37e294b972c6ccd9e348144c2cd37016e246
Pavel Dymov <dpavel82@yandex.ru>
2024-10-01 10:45:37 +0300

    Магазин кристаллов

---
+++ Basics_06_CrystalShop/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Basics_06_CrystalShop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int gold;
+            int crystalPrice = 50;
+            int crystals;
+
+            Console.WriteLine($"Добро пожаловать в нашу лавку!" +
+                $"\nТолько сегодня и тоько у нас магические кристаллы по цене {crystalPrice} золотых!");
+            Console.Write("Сколько у вас золота: ");
+            gold = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Сколько кристаллов вы хотите приобести? Всего {crystalPrice} золотых за штуку: ");
+            crystals = Convert.ToInt32(Console.ReadLine());
+
+            gold -= crystalPrice * crystals;
+            Console.WriteLine($"\nОтлично! Вы приобрели {crystals} шт. У вас осталость {gold} золота.");
+
+            Console.ReadKey();
+        }
+    }
+}
