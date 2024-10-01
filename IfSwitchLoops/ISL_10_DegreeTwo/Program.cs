1edab01dd5b310bb345261baf76384deff516738
Pavel Dymov <dpavel82@yandex.ru>
2024-10-01 15:37:42 +0300

    Степень двойки

---
+++ IfSwitchLoops/ISL_10_DegreeTwo/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ISL_10_DegreeTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int maxRandom = 1000;
+            int randomNumber = random.Next(maxRandom);
+            int number = 1;
+            int power = 0;
+            int baseNumber = 2;
+
+            while (number <= randomNumber)
+            {
+                number *= baseNumber;
+                power++;
+            }
+
+            Console.WriteLine($"Загадано число: {randomNumber}.");
+            Console.WriteLine($"Найдено ближайшее большее число {number}: как {baseNumber} в степени {power}");
+
+            Console.ReadKey();
+        }
+    }
+}
