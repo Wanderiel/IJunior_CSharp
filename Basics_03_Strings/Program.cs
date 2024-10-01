5ec02940978237f4a8e3936f10bbd4c7c569e4cc
Pavel Dymov <dpavel82@yandex.ru>
2024-10-01 10:22:32 +0300

    Работа со строками

---
+++ Basics_03_Strings/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Basics_03_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            string zodiacSign;
+            string post;
+            int age;
+            int salary;
+
+            Console.Write("Здравствуйте! Как вас зовут: ");
+            name = Console.ReadLine();
+
+            Console.Write("Сколько вам полных лет: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Кто вы по знаку зодиака: ");
+            zodiacSign = Console.ReadLine();
+
+            Console.Write("Какая у вас должность: ");
+            post = Console.ReadLine();
+
+            Console.Write("Сколько вы получаете в рублях: ");
+            salary = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\nВаше имя {name}, ваш взраст {age}, по гороскопу вы {zodiacSign}, " +
+                $"занимаете должность {post} и получаете в рублях {salary}.");
+
+            Console.ReadKey();
+        }
+    }
+}
