2b15834bcde5508fb2d9a0b5e3b5568b4fd59d79
Pavel Dymov <dpavel82@yandex.ru>
2024-10-01 11:14:32 +0300

    Поликлиника

---
+++ Basics_07_Polyclinic/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Basics_07_Polyclinic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int queueLength;
+            int durationReception = 10;
+            int minutesInHour = 60;
+            int waitingHours;
+            int waitingMinutes;
+
+            Console.Write("Вы в поликлинике стоите в очереди, сколько перед вами людей: ");
+            queueLength = Convert.ToInt32(Console.ReadLine());
+
+            waitingHours = queueLength * durationReception / minutesInHour;
+            waitingMinutes = queueLength * durationReception % minutesInHour;
+
+            Console.WriteLine($"Вам придётся отстоять в очереди примерно {waitingHours} часов и {waitingMinutes} минут.");
+
+            Console.ReadKey();
+        }
+    }
+}
