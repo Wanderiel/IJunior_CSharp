7eb0c6bd85f19027166dcebf26429b90bf1d3a55
Pavel Dymov <dpavel82@yandex.ru>
2024-10-01 11:43:13 +0300

    Освоение циклов

---
+++ IfSwitchLoops/ISL_01_Mastering/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ISL_01_Mastering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+            int count;
+
+            Console.Write("Повторение - Мать заикания!\nКакое сообщение для вас повторить: ");
+            message = Console.ReadLine();
+
+            Console.Write("Сколько раз это сделать: ");
+            count = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
