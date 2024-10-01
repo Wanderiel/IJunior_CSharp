9ccf24cb51c58d2a6d3c26d9096b552f3050c8a8
Pavel Dymov <dpavel82@yandex.ru>
2024-10-01 13:09:03 +0300

    Оставть один цикл

--- IfSwitchLoops/ISL_03_Sequence/Program.cs
+++ IfSwitchLoops/ISL_03_Sequence/Program.cs
@@ -8,29 +8,12 @@
             int number = firstNumber;
             int step = 7;
             int endNumber = 96;
-            int cyclesCount = 2;
-            Random random = new Random();
 
             Console.WriteLine($"Выводим последовательность чисел от {firstNumber} до {endNumber} с шагом {step}.\n");
 
-            if (random.Next(cyclesCount) == 0)
+            for (int i = firstNumber; i <= endNumber; i += step)
             {
-                Console.WriteLine("Циклом for:");
-
-                for (int i = firstNumber; i <= endNumber; i += step)
-                {
-                    Console.Write($"{i}; ");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Циклом while:");
-
-                while (number <= endNumber)
-                {
-                    Console.Write($"{number}; ");
-                    number += step;
-                }
+                Console.Write($"{i}; ");
             }
 
             Console.ReadKey();
