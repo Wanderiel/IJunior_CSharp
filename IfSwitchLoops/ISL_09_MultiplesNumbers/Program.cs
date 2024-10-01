0715f425be299c5547b015c20b3ab6e65824bfb3
Pavel Dymov <dpavel82@yandex.ru>
2024-10-01 15:27:41 +0300

    Кратные числа

---
+++ IfSwitchLoops/ISL_09_MultiplesNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ISL_09_MultiplesNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int minNumber = 50;
+            int maxNumber = 250;
+            int minRandom = 10;
+            int maxRandom = 25;
+            int number = rand.Next(minRandom, maxRandom);
+            int count = 0;
+
+            for (int i = number; i <= maxNumber; i += number)
+            {
+                if (i >= minNumber && i <= maxNumber)
+                    count++;
+            }
+
+            Console.WriteLine($"Количество чисел в диапазоне [{minNumber}-{maxNumber}] кратных {number}: {count}");
+
+            Console.ReadKey();
+        }
+    }
+}
