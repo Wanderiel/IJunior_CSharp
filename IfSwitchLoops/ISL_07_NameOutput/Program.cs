460b3ec2e13fb052ae5464f909a923241edcfe6c
Pavel Dymov <dpavel82@yandex.ru>
2024-10-01 15:05:14 +0300

    Вывод имени

---
+++ IfSwitchLoops/ISL_07_NameOutput/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ISL_07_NameOutput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            char symbol;
+            string charsString = string.Empty;
+            string nameBetweenSymbol;
+
+            Console.Write("Введите ваше имя: ");
+            name = Console.ReadLine();
+
+            Console.Write("Ведите символ: ");
+            symbol = Convert.ToChar(Console.Read());
+
+            nameBetweenSymbol = symbol + name + symbol;
+
+            for (int i = 0; i < nameBetweenSymbol.Length; i++)
+            {
+                charsString += symbol;
+            }
+
+            Console.WriteLine($"\n{charsString}\n{nameBetweenSymbol}\n{charsString}");
+
+            Console.ReadKey();
+        }
+    }
+}
