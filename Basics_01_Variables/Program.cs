4380832bc4a4f16f6f6d5641ba4a0a9185fad593
Pavel Dymov <dpavel82@yandex.ru>
2024-10-01 09:32:09 +0300

    Добавьте файлы проекта.

---
+++ Basics_01_Variables/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Basics_01_Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Pavel";
+            ushort age = 42;
+            short speed = 6;
+            byte level = 80;
+            sbyte modifer = 9;
+            int lengthSegment = 32000;
+            float hitPoints = 18.9f;
+            double manaPionts = 9.8;
+            char armorGrade = 'A';
+            bool isDead = false;
+        }
+    }
+}
