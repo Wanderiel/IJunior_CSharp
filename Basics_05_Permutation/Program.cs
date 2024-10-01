b877a7d7c306099b917a51ddfa5422d30adc9880
Pavel Dymov <dpavel82@yandex.ru>
2024-10-01 10:35:53 +0300

    Перестановка местами

---
+++ Basics_05_Permutation/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Basics_05_Permutation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Tahlatri";
+            string surname = "Wanderiel";
+            string temp;
+
+            Console.WriteLine(name + " " + surname);
+
+            temp = name;
+            name = surname;
+            surname = name;
+
+            Console.WriteLine(name + " " + surname);
+
+            Console.ReadKey();
+        }
+    }
+}
