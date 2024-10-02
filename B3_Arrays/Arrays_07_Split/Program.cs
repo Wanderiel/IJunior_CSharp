824c25aeb446704bce8a993963245107df392d81
Pavel Dymov <dpavel82@yandex.ru>
2024-10-02 17:18:13 +0300

    Split

---
+++ B3_Arrays/Arrays_07_Split/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Arrays_07_Split
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string quote = "... если цель привлекает, средство должно найтись.";
+            char separator = ' ';
+
+            Console.WriteLine(quote);
+
+            Console.WriteLine();
+
+            foreach (string unit in quote.Split(separator))
+            {
+                Console.WriteLine(unit);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
