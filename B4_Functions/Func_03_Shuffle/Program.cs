4d2604e401ab5b0f3a2f65ec5ec79f9b25116bde
Pavel Dymov <dpavel82@yandex.ru>
2024-10-03 13:46:29 +0300

    Shufle

---
+++ B4_Functions/Func_03_Shuffle/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Func_03_Shuffle
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int size = 30;
+            int[] numbers = new int[size];
+
+            FillArray(numbers);
+
+            Console.WriteLine("Дан массив чисел:");
+
+            PrintArray(numbers);
+
+            Console.WriteLine("\nПосле перемешивания:");
+
+            Shuffle(numbers);
+            PrintArray(numbers);
+
+            Console.ReadKey();
+        }
+
+        private static void FillArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = i;
+        }
+
+        private static void PrintArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+                Console.Write($"{numbers[i]} ");
+
+            Console.WriteLine();
+        }
+
+        private static void Shuffle(int[] numbers)
+        {
+            Random random = new Random();
+            int newIndex;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                newIndex = random.Next(numbers.Length);
+                (numbers[i], numbers[newIndex]) = (numbers[newIndex], numbers[i]);
+            }
+        }
+    }
+}
