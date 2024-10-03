664a748a6d25b250fb3486949d89e868531ce9c5
Pavel Dymov <dpavel82@yandex.ru>
2024-10-03 11:53:37 +0300

    ReadInt

---
+++ B4_Functions/Func_01_ReadInt/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Func_01_ReadInt
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = GetNumber();
+
+            Console.WriteLine(number);
+
+            Console.ReadKey();
+        }
+
+        static private int GetNumber()
+        {
+            int number;
+            string userInput = string.Empty;
+
+            while (int.TryParse(userInput, out number) == false)
+            {
+                Console.Write("Введите число: ");
+                userInput = Console.ReadLine();
+            }
+
+            return number;
+        }
+    }
+}
